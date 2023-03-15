d455cea40d1378e558f141b8c4bc9339553d5438
Jens gerwins <Jens.gerwins@gmail.com>
2023-03-15 22:20:28 +0100

    Light-Switch noch ohne Animation

---
+++ script.js
@@ -0,0 +1,10 @@
+const button = document.querySelector("button");
+const bodys =document.querySelector("body");
+
+button.addEventListener("click", () => { 
+    
+   //console.log("hi");
+button.classList.toggle("button-red");
+bodys.classList.toggle("bo1");
+});
+
